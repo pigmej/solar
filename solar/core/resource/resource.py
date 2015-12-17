212d38a2a1a3187c85e4454d9cbd3523c47db79b
Jedrzej Nowak <jnowak@mirantis.com>
2015-12-17 15:26:08 +0100

    Fixed pep8 in resource.py
    Change-Id: I414401e87fc6ac233cf7c1c428c268fee1411fce

--- solar/core/resource/resource.py
+++ solar/core/resource/resource.py
@@ -25,7 +25,8 @@ import networkx
 
 
 from solar.core.signals import get_mapping
-from solar.core.resource.repository import Repository, read_meta
+from solar.core.resource.repository import read_meta
+from solar.core.resource.repository import Repository
 from solar.core import validation
 from solar.dblayer.model import StrInt
 from solar.dblayer.solar_models import CommitedResource
